c6fad2fa804bddd5e20805ee51a736c0b0f2785a
dPfla <choyr2016@naver.com>
2021-03-01 03:15:09 +0900

    rawData에 담긴 신도림동 list위치 찾기

--- Python-Data Parsing/Day03 신도림동 찾기.py
+++ Python-Data Parsing/Day03 신도림동 찾기.py
@@ -1,6 +1,6 @@
 import csv
 
-f = open('C:/Users/user/Desktop/github/Python/Study-Python/Python-Solve/자료/연령별인구현황_월간.csv')
+f = open("C:/Users/user/Desktop/github/Python/자료/연령별인구현황_월간.csv")
 data = csv.reader(f)
 
 rawData = []
